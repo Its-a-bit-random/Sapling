69c79aa8117e63c4852fb608843a0a7bc6fbb1dd
Sebastian2852 <sebastian2852@proton.me>
2026-04-02 19:02:21 +0200

    Setup package

--- bin/build.sh
+++ bin/build.sh
@@ -5,3 +5,4 @@ rojo build packages/sapling -o build/sapling.rbxm
 rojo build packages/lifelikeCharactersService -o build/lifelikeCharactersService.rbxm
 rojo build packages/queryBuilder -o build/queryBuilder.rbxm
 rojo build packages/registry -o build/registry.rbxm
+rojo build packages/sweeper -o build/sweeper.rbxm
